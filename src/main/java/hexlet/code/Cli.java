338ac8e2d2579061ba018a18ce2704a26153bac4
Veronika Avdeeva <avdeevaveronika@gmail.com>
2022-04-25 23:20:35 +0300

    step 2

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void greeting() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println("Hello, " + str + "!");
+    }
+
+}
