ad29fe1e837c4bb6892ba49f9ac2685e1b7919d4
Veronika Avdeeva <avdeevaveronika@gmail.com>
2022-05-18 22:25:16 +0300

    step 7

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
@@ -30,6 +32,9 @@ public class App {
                 break;
             case "3":
                 Calc.runGame();
+                break;
+            case "4":
+                GCD.runGame();
                 break;
             default:
         }
---
+++ src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,41 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+
+public class GCD {
+    public static void runGame() {
+        String rule = "Find the greatest common divisor of given numbers.";
+        String name = Engine.greeting(rule);
+
+        boolean hasError = false;
+
+        for (int i = 0; i < Calc.NUMBER_OF_TRY; i++) {
+            Random rand = new Random();
+            int number1 = rand.nextInt(1, Calc.MAX_VALUE);
+            int number2 = rand.nextInt(1, Calc.MAX_VALUE);
+
+            int min = Integer.min(number1, number2);
+            int gcd = 1;
+            for (int j = 1; j <= min; j++) {
+                if (number1 % j == 0 && number2 % j == 0) {
+                    gcd = j;
+                }
+            }
+
+            String rightAnswer = Integer.toString(gcd);
+
+            String question = number1 + " " + number2;
+
+            if (Engine.isNotEqualWithRightAnswer(question, rightAnswer, name)) {
+                hasError = true;
+                break;
+            }
+        }
+
+        if (!hasError) {
+            System.out.println("Congratulations, " + name + "!");
+        }
+    }
+}
